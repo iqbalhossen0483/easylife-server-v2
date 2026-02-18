9adcff60d6f0ae35adb3071ed5db09ef2b7ad920
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-02-18 18:03:00 +0600

    user module added

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,6 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TenantDatabaseService } from 'src/database/tenant-datasource.manager';
+import { DbListEntity } from 'src/entites/dbList.entity';
+import { UserEntity } from 'src/entites/user.entity';
+import { FindOptionsWhere, Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
-  constructor() {}
+  constructor(
+    private readonly tenantDatabaseService: TenantDatabaseService,
+    @InjectRepository(DbListEntity)
+    private readonly dbListRepo: Repository<DbListEntity>,
+  ) {}
+
+  private async getDataDatabase(tenantId: number) {
+    const database = await this.dbListRepo.findOne({ where: { id: tenantId } });
+    if (!database) {
+      throw new UnauthorizedException('No organization found');
+    }
+
+    return database;
+  }
+
+  private async getUser(condition: FindOptionsWhere<UserEntity>) {
+    const userRepo = await this.tenantDatabaseService.getRepository(UserEntity);
+    const user = await userRepo.findOne({ where: condition });
+
+    if (!user) {
+      throw new UnauthorizedException('No user found');
+    }
+
+    return user;
+  }
 }
