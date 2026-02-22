87d60e185d86632a3bf7fc1c4447fba35ec752de
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-02-22 12:08:50 +0600

    expo service added

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,8 +1,42 @@
-import Expo from 'expo-server-sdk';
+import { Injectable } from '@nestjs/common';
+import Expo, { ExpoPushMessage } from 'expo-server-sdk';
 
+type NotificationParams = {
+  tokens: string[];
+  title: string;
+  body: string;
+  data: Record<string, unknown>;
+};
+
+@Injectable()
 export class NotificationService {
   expo: Expo;
   constructor() {
     this.expo = new Expo();
+  }
+
+  async sendNotification({ tokens, title, body, data }: NotificationParams) {
+    const messages: ExpoPushMessage[] = [];
+    for (const pushToken of tokens) {
+      const isValid = Expo.isExpoPushToken(pushToken);
+      if (!isValid) {
+        console.error(`This token is invalid Expo push token`);
+        continue;
+      }
+
+      messages.push({
+        to: pushToken,
+        sound: 'default',
+        title: title,
+        body: body,
+        data: data,
+      });
+    }
+
+    const chunks = this.expo.chunkPushNotifications(messages);
+
+    for (const chunk of chunks) {
+      await this.expo.sendPushNotificationsAsync(chunk);
+    }
   }
 }
